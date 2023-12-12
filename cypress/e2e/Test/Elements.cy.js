2a2c97a55530d92a230238c70f67b3708e8fdf81
Rancer Ventura <r.2vm@hotmail.com>
2023-12-11 21:50:45 -0400

    adding specs changes and assertions

--- cypress/e2e/Test/Elements.cy.js
+++ cypress/e2e/Test/Elements.cy.js
@@ -1,6 +1,8 @@
 import Elements from "C:/Users/r2vm/OneDrive/Escritorio/All/Code/Codes/Cypress/demoqa/pageObjectsSelectors/elementsObjects.js"
 
  const elem = new Elements()
+ const h5 = "Following links will open new tab"
+ const badRequestTex = "Link has responded with staus 400 and status text Bad Request"
 
 describe('Visit demoQA', () => {
   beforeEach(() => {
@@ -8,24 +10,64 @@ describe('Visit demoQA', () => {
     elem.clickElements()
   })
 
-  it('Go to check box', () => {
-      elem.goToCheckBox()
+  it('Go to checkbox', () => {
+    elem.goToCheckBox()
+    elem.element.homeTitle().should("contain", "Home")
+  })
+
+  it('Click home checkbox', () => {
+      elem.clickHomeCheckBox()
+      elem.element.homeText().should("contain", "You have selected")
+      elem.unclickHomeCheckBox()
+    })
+
+  it('Click desktop checkbox', () => {
+      elem.clickDesktopCheckBox()
+      elem.element.desktopText().should("contain", "desktop")    
     })
 
   it('Go to Web Tables', () => {
       elem.goToWebTables()
+      elem.element.firstNameHeader().should("exist")
+    })
+
+  it('Making a search', () => {
+      elem.goToWebTables()
+      elem.searching("Vega")
+      elem.element.lastNameSecondCollum().should("not.have.text")
     })
 
   it('Go to Buttons', () => {
       elem.goToButtons()
+      elem.element.doubleClickMeBtn().should("be.visible")
+    })
+
+  it('Double clicking', () => {
+      elem.goToButtons()
+      elem.dubleClick()
+      elem.element.doubleClickMessage().should("match", "#doubleClickMessage")
     })
 
   it('Go to Links', () => {
       elem.goToLinks()
+      elem.element.linksH5().should("have.text", h5)
+    })
+
+  it('Bad request', () => {
+      elem.goToLinks()
+      elem.clickBadRquestBtn()
+      elem.element.badRequestMessage().should("contain", "400")
+      .request({
+        url: 'https://demoqa.com/__/#/specs/runner?file=cypress/e2e/Test/Elements.cy.js',
+        failOnStatusCode:false,
+    }).then((resp) => {
+        expect(resp.status).to.eq(200)
+    })
     })
 
   it('Go to Broken Link - Images', () => {
       elem.goToBrokenLinksImages()
+      elem.element.brokenImageP().should("contain", "Broken image")
     })
 
   it('Go to Upload and Download', () => {
--- pageObjectsSelectors/elementsObjects.js
+++ pageObjectsSelectors/elementsObjects.js
@@ -3,11 +3,27 @@ class Elements {
     element ={
         elementsBtn : () => cy.get(":nth-child(1) > :nth-child(1) > .card-body"),
         checkBoxBtn : () => cy.get(":nth-child(1) > .element-list > .menu-list > #item-1"),
+        homeTitle : () => cy.get(".rct-title"),
+        checkboxDropd : () => cy.get(".rct-collapse"),
+        homeCheckBox : () => cy.get(".rct-checkbox"),
+        homeText : () => cy.get("#result"),
+        desktopCheckBox : () => cy.get(".rct-node-expanded > ol > :nth-child(1) > .rct-text > label > .rct-checkbox"),
+        desktopText : () => cy.get("#result > :nth-child(2)"),
         radioButtonBtn : () => cy.get(":nth-child(1) > .element-list > .menu-list > #item-2"),
         webTablesBtn : () => cy.get(":nth-child(1) > .element-list > .menu-list > #item-3"),
+        firstNameHeader : () => cy.get(":nth-child(1) > .rt-resizable-header-content"),
+        searchBar : () => cy.get("#searchBox"),
+        searchButton : () => cy.get("#basic-addon2"),
+        lastNameSecondCollum : () => cy.get(":nth-child(2) > .rt-tr > :nth-child(2)"),
         buttonsBtn : () => cy.get(":nth-child(1) > .element-list > .menu-list > #item-4"),
+        doubleClickMeBtn : () => cy.get("#doubleClickBtn"),
+        doubleClickMessage : () => cy.get("#doubleClickMessage"),
         linksBtn : () => cy.get(":nth-child(1) > .element-list > .menu-list > #item-5"),
+        linksH5 : () => cy.get(":nth-child(1) > strong"),
+        badRequestBtn : () => cy.get("#bad-request"),
+        badRequestMessage : () => cy.get("#linkResponse > :nth-child(1)"),
         brokenLinksImagesBtn : () => cy.get(":nth-child(1) > .element-list > .menu-list > #item-6"),
+        brokenImageP : () => cy.get(":nth-child(2) > :nth-child(5)"),
         uploadDownloadBtn : () => cy.get(":nth-child(1) > .element-list > .menu-list > #item-7"),
         dynamicPropertiesBtn : () => cy.get(":nth-child(1) > .element-list > .menu-list > #item-8"),
     }
@@ -20,6 +36,21 @@ class Elements {
         this.element.checkBoxBtn().click();
     }
 
+    clickHomeCheckBox(){
+        this.element.checkBoxBtn().click();
+        this.element.homeCheckBox().click(); 
+    }
+
+    unclickHomeCheckBox(){
+        this.element.homeCheckBox().click();
+    }
+
+    clickDesktopCheckBox(){
+        this.element.checkBoxBtn().click();
+        this.element.checkboxDropd().click();
+        this.element.desktopCheckBox().click(); 
+    }
+
     goToRadioButton(){
         this.element.radioButtonBtn().click();
     }
@@ -28,12 +59,25 @@ class Elements {
         this.element.webTablesBtn().click();
     }
 
+    searching(item){
+        this.element.searchBar().type(item);
+        this.element.searchButton().click();
+    }
+
     goToButtons(){
         this.element.buttonsBtn().click();
     }
 
+    dubleClick(){
+        this.element.doubleClickMeBtn().dblclick();
+    }
+
     goToLinks(){
         this.element.linksBtn().click();
+    }
+
+    clickBadRquestBtn(){
+        this.element.badRequestBtn().click();
     }
     
     goToBrokenLinksImages(){
